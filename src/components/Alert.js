c89023491e977327d690fe25344c7fbd2631bb60
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-05-28 21:23:50 +0530

    level2

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,7 +6,7 @@ export default function Alert (props) {
         return lower.charAt(0).toUpperCase() + lower.substr(1)
     }
     return (
-        props.alert && <div className={`alert alert-${props.alert.type} z-3 float-end mx-4`} role="alert">
+        props.alert && <div className={`alert alert-${props.alert.type}`} role="alert">
             <strong>{capitalise(props.alert.type)}</strong> {props.alert.msg}.
         </div>
     )
